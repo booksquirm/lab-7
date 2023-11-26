756a8abbf87a62e44fd1e2094453765037428ac8
Jen Nino <jenknino@gmail.com>
2023-11-26 13:58:18 -0800

    Ammend JS to randomize image pulled from API

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ fetch('https://dog-api.kinduff.com/api/facts?number=1')
     factElement.textContent = data["facts"]
     document.body.appendChild(factElement);
   });
+  
+  const index = Math.floor(Math.random() * 19);
 
   fetch('https://pixabay.com/api/?key=26114851-dca9c69ca52910081b2dabdc2&q=dogs&image_type=photo')
   .then(response => response.json())
   .then(data => {
-    picUrl = data["hits"][0]["previewURL"];
+    picUrl = data["hits"][index]["previewURL"];
     console.log(picUrl);
     const elem = document.getElementById("dog-image");
     console.log(elem);
