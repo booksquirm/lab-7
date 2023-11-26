660567cb16ea67394bafbfa184289a0d4dbec717
Jen Nino <jenknino@gmail.com>
2023-11-26 13:52:45 -0800

    Fix JS code

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ fetch('https://dog-api.kinduff.com/api/facts?number=1')
   fetch('https://pixabay.com/api/?key=26114851-dca9c69ca52910081b2dabdc2&q=dogs&image_type=photo')
   .then(response => response.json())
   .then(data => {
-    const factElement = document.createElement('p');
-    factElement.textContent = data["hits"][0]["pageURL"];
-    document.body.appendChild(factElement);
+    picUrl = data["hits"][0]["previewURL"];
+    console.log(picUrl);
+    const elem = document.getElementById("dog-image");
+    console.log(elem);
+    elem.src = picUrl;
   });
